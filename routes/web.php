27873aab375e986f7a7d5bd018f3931af0fb2e2a
rifkias <rifkialfarizshidiq.1@gmail.com>
2023-05-30 00:15:14 +0700

    Set Default Response Web

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/',function(){
+    return response("Welcome",200)->header('Content-Type', 'text/plain');
 });
